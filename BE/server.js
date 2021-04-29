4ba0d92d961c59caa565a30494514b1d05aa7818
Viet Anh Le <mc.vietanh@gmail.com>
2021-04-29 09:41:56 -0400

    done connect to mongoDB

---
+++ BE/server.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+// Connect to mongodb server
+
+const URI = process.env.MONGODB_URL;
+
+mongoose.connect(
+  URI,
+  {
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log('Connected to mongodb');
+  }
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.json({ msg: 'Hello World!' });
+});
+
+app.listen(PORT, () => {
+  console.log('Server is running on port', PORT);
+});
